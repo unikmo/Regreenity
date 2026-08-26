1f251b912e36580f175d074bd3804975f7a9541b
unikmo <mbanwie@googlemail.com>
2026-08-26 23:01:40 +0200

    feat: route public site to Regreenity marketing experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import MarketingSite from './MarketingSite'
 import './styles.css'
+import './marketing.css'
 import { registerOfflineShell } from './offline'
 
 registerOfflineShell()
 
+const useLegacyProductShell = window.location.pathname.startsWith('/product-app/')
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {useLegacyProductShell ? <App /> : <MarketingSite />}
   </React.StrictMode>,
 )
