b448a03a0c594e0f42aeafcb347ea931b33b2c22
unikmo <mbanwie@googlemail.com>
2026-08-28 14:06:13 +0200

    Add Amara name badge to hero

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'regreenity-shell-v4'
+const CACHE = 'regreenity-shell-v5'
 const SHELL = [
   '/',
   '/passenger-experience/',
@@ -15,7 +15,7 @@ const SHELL = [
   '/cookies/',
   '/site.webmanifest',
   '/favicon.svg',
-  '/media/hero-centered-black-crew.jpg',
+  '/media/hero-centered-black-crew-amara.jpg',
   '/media/crew-recognition.jpg',
   '/media/passenger-connection.jpg',
   '/media/service-recovery.jpg',
