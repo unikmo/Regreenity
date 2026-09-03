68e43bd29efff7ddd09cf2cc834305d7e78b5f67
unikmo <mbanwie@googlemail.com>
2026-09-03 11:43:33 +0200

    Route and footer-link resort test page

--- src/main.tsx
+++ src/main.tsx
@@ -9,14 +9,28 @@ const useLegacyProductShell = path.startsWith('/product-app/')
 const useLegacyLegalShell = ['/imprint/', '/privacy/', '/terms/', '/cookies/'].some((route) => path.startsWith(route))
 const usePortal = path.startsWith('/portal/')
 const useSandbox = path.startsWith('/sandbox/')
+const useResort = path.startsWith('/all-inclusive-resorts/')
 
 const loadRoute = async () => {
   if (useSandbox) return Promise.all([import('./Sandbox'), import('./sandbox.css')]).then(([module]) => module.default)
   if (usePortal) return import('./Portal').then(module => module.default)
   if (useLegacyProductShell) return Promise.all([import('./ExecutiveWalkthrough'), import('./executive-walkthrough.css')]).then(([module]) => module.default)
   if (useLegacyLegalShell) return Promise.all([import('./App'), import('./styles.css')]).then(([module]) => module.default)
+  if (useResort) return Promise.all([import('./ResortSite'), import('./marketing.css'), import('./resort.css')]).then(([module]) => module.default)
   return Promise.all([import('./MarketingSite'), import('./marketing.css')]).then(([module]) => module.default)
 }
 
 const Route = await loadRoute()
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><Route /></React.StrictMode>)
+
+if (!useLegacyProductShell && !useLegacyLegalShell && !usePortal && !useSandbox) {
+  requestAnimationFrame(() => {
+    const footer = document.querySelector<HTMLElement>('.footer-links')
+    if (!footer || footer.querySelector('a[href="/all-inclusive-resorts/"]')) return
+    const link = document.createElement('a')
+    link.href = '/all-inclusive-resorts/'
+    link.textContent = 'All-Inclusive Resorts'
+    link.dataset.resortPreview = 'true'
+    footer.prepend(link)
+  })
+}
